bff6d6ef1c64d836fcee44f9dfa241a6c4d01104
Shash-Rao <shashwat.a.rao@gmail.com>
2021-08-29 15:26:29 -0400

    Commit 2

--- app/src/main/java/com/raoshashwat/lab01/MainActivity.java
+++ app/src/main/java/com/raoshashwat/lab01/MainActivity.java
@@ -2,6 +2,8 @@ package com.raoshashwat.lab01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,10 +13,14 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     Button incrementButton;
+    Button decrementButton;
     TextView greetingDisplay;
     int count = 0;
+    Random rand = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +34,17 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("incrementing " + ++count);
                 Log.i("incrementing", count + "");
                 greetingDisplay.setText(count + "");
+                int col = Color.argb(255, rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+                incrementButton.setBackgroundTintList(ColorStateList.valueOf(col));
             }
         });
     }
 
     public void decrement(View view) {
+        decrementButton = findViewById(R.id.decrement_button);
         System.out.println("incrementing " + --count);
         greetingDisplay.setText(count + "");
+        int col = Color.argb(255, rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+        decrementButton.setBackgroundTintList(ColorStateList.valueOf(col));
     }
 }
